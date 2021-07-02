74bec4c2bb8447c634e2bd38ea5596335cf62f0d
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-02 19:58:53 -0300

    Tela de boas vindas

---
+++ src/views/mystore/BemVindo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const BemVindo = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Bem Vindo</Text>
+        </View>
+    )
+}
+
+export default BemVindo;
