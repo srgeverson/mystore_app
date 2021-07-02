20b36bdfba2325bb2d64dbf63e7d2331a0bf9f95
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-02 19:59:18 -0300

    Tela de recuperar senha

---
+++ src/views/usuario/RecuperarSenha/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RecuperarSenha = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Recuperar Senha</Text>
+        </View>
+    )
+}
+
+export default RecuperarSenha;
