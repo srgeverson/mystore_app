98b3c7d95a598a481981c2755f962454267bedeb
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-03 20:14:30 -0300

    Configurações de estilos glogais

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    colors: {
+        primary: '#007bff',
+        secondary: '#6c757d',
+        success: '#28a745',
+        info: '#17a2b8',
+        warning: '#ffc107',
+        danger: '#dc3545',
+        light: '#f8f9fa',
+        dark: '#343a40',
+    },
+    sizes: {
+        icon: 15
+    },
+    margins: {
+        iconTextLeft: 5,
+        iconTextRight:5
+    }
+}
