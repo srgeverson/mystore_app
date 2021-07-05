7ec7ebd753d054c7d7b60c2ce5d583b1e369356e
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-04 21:55:26 -0300

    Tela de recuperar senha

--- src/views/usuario/RecuperarSenha/index.js
+++ src/views/usuario/RecuperarSenha/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Card, Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import logo from '../../../assets/images/logo.png';
+import BotaoConfirmar from '../../../components/BotaoConfirmar';
 
 const RecuperarSenha = () => {
+
+    const navigation = useNavigation();
+
+    const recuperarSenha = () => {
+
+        navigation.navigate('Login');
+    }
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Recuperar Senha</Text>
-        </View>
+        <>
+            <View>
+                <Card>
+                    <Card.Title>Recuperar Senha</Card.Title>
+                    <Card.Divider />
+                    <Card.Image source={logo}>
+                    </Card.Image>
+                    <Input
+                        placeholder="Digite seu email aqui"
+                        leftIcon={<Icon name="envelope" size={18}/>}
+                    //onChangeText={value => setEmail(value)}
+                    //errorMessage={!email && !menssagemDeErro ? 'Usuário é obrigatório!' : ''}
+                    />
+                    <BotaoConfirmar titulo="Trocar" pressionado={recuperarSenha} />
+                </Card>
+            </View>
+        </>
     )
 }
 
