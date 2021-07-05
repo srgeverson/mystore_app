dd8870dddb48973320e34ef77a2ffef2c7bb59db
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-04 21:46:33 -0300

    Botão confirmar

---
+++ src/components/BotaoConfirmar/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { theme } from '../../assets/styles/theme';
+
+const BotaoConfirmar = (props) => {
+    return (
+        <>
+            <Button
+                title={props.titulo}
+                onPress={props.pressionado}
+                icon={
+                    <Icon
+                        name="check"
+                        size={theme.sizes.icon}
+                        color={theme.colors.light}
+                        style={{ marginRight: theme.margins.iconTextRight }} />
+                }
+                style={{ color: theme.colors.primary }}
+            />
+        </>
+    )
+}
+export default BotaoConfirmar;
