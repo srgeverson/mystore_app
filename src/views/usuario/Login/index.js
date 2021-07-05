07fb7e727b3e50c37bfc30ce0c0034bbcbc1d4b4
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-04 22:24:26 -0300

    Removendo os estilos do containner, pois será usado o componente Card.

--- src/views/usuario/Login/index.js
+++ src/views/usuario/Login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Input, Image, Text } from 'react-native-elements';
+import { Input, Card, Text } from 'react-native-elements';
 import logo from '../../../assets/images/logo.png';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
@@ -46,28 +46,37 @@ const Login = () => {
 
     const validarAcesso = () => {
         navigation.navigate('PrimeiroAcesso');
-    }    
+    }
 
     return (
-        <View style={styles.container}>
-            <Image source={logo} style={styles.logo} />
-            <Input
-                placeholder="Digite seu email aqui"
-                onChangeText={value => setEmail(value)}
-                errorMessage={!email && !menssagemDeErro ? 'Usuário é obrigatório!' : ''} />
+        <>
+            <View>
+                <Card>
+                    <Card.Image source={logo} style={styles.logo}>
+                    </Card.Image>
+                    <Input
+                        placeholder="Digite seu email aqui"
+                        onChangeText={value => setEmail(value)}
+                        errorMessage={!email && !menssagemDeErro ? 'Usuário é obrigatório!' : ''}
+                        leftIcon={<Icon name="envelope" size={18} />} />
 
-            <Input
-                placeholder='Digite sua senha aqui'
-                onChangeText={value => setSenha(value)}
-                errorMessage={!senha && !menssagemDeErro ? 'Senha é obrigatório!' : ''}
-                secureTextEntry={true} />
+                    <Input
+                        placeholder='Digite sua senha aqui'
+                        onChangeText={value => setSenha(value)}
+                        errorMessage={!senha && !menssagemDeErro ? 'Senha é obrigatório!' : ''}
+                        secureTextEntry={true}
+                        leftIcon={<Icon name="key" size={18} />} />
 
-            <BotaoEntrar titulo="Entrar" pressionado={entrar} />
-            <View style={styles.cadastro}>
-                <Text style={styles.recuperarSenha} onPress={recuperarSenha}>Esqueceu a senha?</Text>
-                <Text style={styles.validarAcesso} onPress={validarAcesso}>Primeiro acesso?</Text>
+                    <BotaoEntrar titulo="Entrar" pressionado={entrar} />
+                    <Card.Divider />
+                    <Card.Title>
+                        <Text style={styles.recuperarSenha} onPress={recuperarSenha}>Esqueceu a senha?</Text>
+                        {' '}
+                        <Text style={styles.validarAcesso} onPress={validarAcesso}>Primeiro acesso?</Text>
+                    </Card.Title>
+                </Card>
             </View>
-        </View>
+        </>
     )
 }
 
