9da650480204bdc9f7990e27ca99f8e25519dacf
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-04 22:24:42 -0300

    Implementoando o componente card

--- src/views/usuario/Login/styles.js
+++ src/views/usuario/Login/styles.js
@@ -3,30 +3,16 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../../assets/styles/theme';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: theme.colors.light
-    },
     logo: {
-        paddingBottom: 20,
         width: 200,
         height: 150
     },
-    cadastro: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-    },
     recuperarSenha: {
         color: theme.colors.primary,
-        margin: 5,
         fontSize: 15,
     },
     validarAcesso: {
         color: theme.colors.primary,
-        margin: 5,
         fontSize: 15,
     }
 });
