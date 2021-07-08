f4d544270615a76116563ee08b78d1572b153cf8
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-08 00:40:48 -0300

    Spinner ao clicar no botão

--- src/components/BotaoEntrar/index.js
+++ src/components/BotaoEntrar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { theme } from '../../assets/styles/theme';
@@ -10,12 +11,16 @@ const BotaoEntrar = (props) => {
                 title={props.titulo}
                 onPress={props.pressionado}
                 icon={
-                    <Icon
-                        name="unlock"
-                        size={theme.sizes.icon}
-                        color={theme.colors.light}
-                        style={{ marginRight: theme.margins.iconTextRight }} />
+                    props.carregando ?
+                        <ActivityIndicator size={theme.sizes.icon} color={theme.colors.light} />
+                        :
+                        <Icon
+                            name="unlock"
+                            size={theme.sizes.icon}
+                            color={theme.colors.light}
+                            style={{ marginRight: theme.margins.iconTextRight }} />
                 }
+                disabled={props.carregando}
             />
         </>
     )
