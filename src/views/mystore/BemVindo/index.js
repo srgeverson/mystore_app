78a260afd63afba63a8a772f2be4642de7e1f6e9
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-08 00:41:51 -0300

    Adicionando a barra de ferramentas

--- src/views/mystore/BemVindo/index.js
+++ src/views/mystore/BemVindo/index.js
@@ -1,10 +1,29 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Header, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const BemVindo = () => {
+
+    const navigation = useNavigation();
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Bem Vindo</Text>
+        <View>
+            <Header
+                placement="center"
+                leftComponent={
+                    <Button
+                        icon={
+                            <Icon
+                                onPress={() => navigation.openDrawer()}
+                                name="bars"
+                                size={20}
+                                color='#FFF' />}
+                    />}
+                centerComponent={{ text: 'My Store', style: { color: '#fff', fontSize: 20 } }}
+            //rightComponent={{ icon: 'home', color: '#fff' }} 
+            />
         </View>
     )
 }
