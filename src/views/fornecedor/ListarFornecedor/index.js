611fe39d71e8fee3230b2b0ae7f53e06c572fc7d
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-18 09:13:31 -0300

    View Listar fonecedor

---
+++ src/views/fornecedor/ListarFornecedor/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const ListarFornecedor = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>ListarFornecedor</Text>
+        </SafeAreaView>
+    )
+}
+
+export default ListarFornecedor;
