3e9e52c56fae97a119ed90f62c4f2aa269c43db1
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-18 09:13:54 -0300

    trocando coponente para safeareaview

--- src/views/mystore/BemVindo/index.js
+++ src/views/mystore/BemVindo/index.js
@@ -1,15 +1,18 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { Header, Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const BemVindo = () => {
 
+    const offline = useState(true);
+
     const navigation = useNavigation();
 
     return (
-        <View>
+        <SafeAreaView>
             <Header
                 placement="center"
                 leftComponent={
@@ -22,9 +25,24 @@ const BemVindo = () => {
                                 color='#FFF' />}
                     />}
                 centerComponent={{ text: 'My Store', style: { color: '#fff', fontSize: 20 } }}
-            //rightComponent={{ icon: 'home', color: '#fff' }} 
+                rightComponent={<Button
+                    icon={
+                        offline ?
+                            <Icon
+                                onPress={() => Alert.alert('Dispositivo off-line', 'Dados serão sincronizados com o servidor quando voltar a internet!')}
+                                name="warning"
+                                size={20}
+                                color='#FFFF00' />
+                            :
+                            <Icon
+                                onPress={() => Alert.alert('Dispositivo on-line', 'Dados estão sincronizados com o servidor!')}
+                                name="refresh"
+                                size={20}
+                                color='#90EE90' />
+                    }
+                />}
             />
-        </View>
+        </SafeAreaView>
     )
 }
 
