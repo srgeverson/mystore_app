42586f3d9ed33c9ee20dc0b0aa67aca838ead0ee
Geverson Souza <geversonjosedesouza@gmail.com>
2021-07-18 09:11:52 -0300

    Configuração do menu drawer

--- src/core/routes/DrawerNavigator.js
+++ src/core/routes/DrawerNavigator.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { BemVindoStackNavigator } from './StackNavigator';
+import React, { useContext } from 'react';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { BemVindoStackNavigator, ListarFornecedorStackNavigator } from './StackNavigator';
+import { AuthorityContext } from '../contexts';
 
 const Drawer = createDrawerNavigator();
 
+const DrawerCustom = (props) => {
+
+    const { signOut } = useContext(AuthorityContext);
+
+    return (
+        <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+            <DrawerItem label="Sair" onPress={() => signOut()} />
+        </DrawerContentScrollView>
+    );
+}
+
 const DrawerNavigator = () => {
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator drawerContent={props => <DrawerCustom {...props} />}>
             <Drawer.Screen name="BemVindo" component={BemVindoStackNavigator} options={{ headerShown: false, }} />
+            <Drawer.Screen name="ListarFornecedor" component={ListarFornecedorStackNavigator} options={{ headerShown: false, }} />
         </Drawer.Navigator>
     );
 };
