802301dda3b5785ac0d6cfea70954c1aeec39d11
˜GeversonSouza <geversonjosedesouza@gmail.com>
2021-11-28 20:37:49 -0300

    Remoção de importação global do base64.

--- src/core/app/index.js
+++ src/core/app/index.js
@@ -1,10 +1,6 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import Routes from '../routes';
-//Importacao global do base64
-import {decode, encode} from 'base-64';
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode } 
 
 const App = () => {
     return (
