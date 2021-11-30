875c1a22d9041d9a5e0bfcb70eb092bd3ec333de
˜GeversonSouza <geversonjosedesouza@gmail.com>
2021-11-30 00:40:17 -0300

    geração do token de recuperação de senha.

--- src/core/routes/DrawerNavigator.js
+++ src/core/routes/DrawerNavigator.js
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { BemVindoStackNavigator, ListarFornecedorStackNavigator } from './StackNavigator';
 import { AuthorityContext } from '../contexts';
+import { rootEntryPoint } from '../../services/UsuarioService';
 
 const Drawer = createDrawerNavigator();
 
@@ -18,10 +19,29 @@ const DrawerCustom = (props) => {
 }
 
 const DrawerNavigator = () => {
+
+    const [menusDisponiveis, setMenusDisponiveis] = useState([]);
+
+    const getEndpoints = async () => {
+        const entryPoint = await rootEntryPoint();
+        setMenusDisponiveis([entryPoint._links]);
+    }
+
+    useEffect(() => {
+        getEndpoints();
+        //console.log(`Capturando endpoins...${JSON.stringify(menusDisponiveis)}`);
+    }, []);
+
     return (
-        <Drawer.Navigator drawerContent={props => <DrawerCustom {...props} />}>
-            <Drawer.Screen name="BemVindo" component={BemVindoStackNavigator} options={{ headerShown: false, }} />
-            <Drawer.Screen name="ListarFornecedor" component={ListarFornecedorStackNavigator} options={{ headerShown: false, }} />
+        <Drawer.Navigator screenOptions={{
+            drawerStyle: {
+                backgroundColor: '#000',
+                width: 240,
+            },
+            drawerStatusBarAnimation: 'fade'
+        }} drawerContent={props => <DrawerCustom {...props} />}>
+            <Drawer.Screen name="BemVindo" component={BemVindoStackNavigator} options={{ headerShown: true, title: 'MySore' }} />
+            <Drawer.Screen name="ListarFornecedor" component={ListarFornecedorStackNavigator} options={{ headerShown: true, }} />
         </Drawer.Navigator>
     );
 };
--- src/services/UsuarioService.js
+++ src/services/UsuarioService.js
@@ -1,5 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { api } from '../core/api';
+import { api, authorizationServerRecuperarSenha } from '../core/api';
+
+const versao = '1';
 
 export const getTokenLogin = async () => {
     try {
@@ -15,15 +17,15 @@ export const getTokenLogin = async () => {
         const dataExpiresMilisegundos = expiresMilisegundos + dataTokenMilisegundos;
         const dataAtualMilisegundos = new Date().getTime();
         const dataRestanteMilisegundos = dataExpiresMilisegundos - dataAtualMilisegundos;
-        console.log(`
-                     data=${data} 
-                     dataTokenMilisegundos=${dataTokenMilisegundos} 
-                     expires=${expires} 
-                     expiresMilisegundos=${expiresMilisegundos}
-                     dataExpiresMilisegundos=${dataExpiresMilisegundos}
-                     dataAtualMilisegundos=${dataAtualMilisegundos}
-                     dataRestanteMilisegundos=${dataRestanteMilisegundos / 1000 / 69 / 69}
-                     `)
+        // console.log(`
+        //              data=${data} 
+        //              dataTokenMilisegundos=${dataTokenMilisegundos} 
+        //              expires=${expires} 
+        //              expiresMilisegundos=${expiresMilisegundos}
+        //              dataExpiresMilisegundos=${dataExpiresMilisegundos}
+        //              dataAtualMilisegundos=${dataAtualMilisegundos}
+        //              dataRestanteMilisegundos=${dataRestanteMilisegundos / 1000 / 69 / 69}
+        //              `);
         if (token !== null && dataRestanteMilisegundos > 0) {
             return token;
         } else {
@@ -31,6 +33,31 @@ export const getTokenLogin = async () => {
         }
     } catch (error) {
         console.log(`Erro no método getTokenLogin do arquivo UsuarioService -> ${new Date()} -> erro: ${error}`);
+    }
+}
+
+export const recuperarSenha = async (uri) => {
+    const token = await authorizationServerRecuperarSenha();
+    try {
+        return await api(token.access_token)
+            .get(`/v${versao}${uri}`)
+            .then((respose) => {
+                if (respose)
+                    return { codigo: 204, };
+            }).catch((error) => {
+                console.log(`Erro na requisição da API andpoint codigo-acesso! Erro: ${error}`);
+                if (error.response) {
+                    return {
+                        codigo: error.response.status,
+                        erro: error.response.error ? error.response.error : error.title,
+                        mensagem: error.response.error_description ? error.response.error_description : error.detail,
+                    }
+                } else {
+                    throw error;
+                }
+            });
+    } catch (error) {
+        console.log(`Erro no método codigo-acesso do arquivo UsuarioService -> ${new Date()} -> erro: ${error}`);
     }
 }
 
@@ -44,6 +71,32 @@ export const limparTokenLogin = async () => {
     }
 }
 
+export const rootEntryPoint = async () => {
+    const token = await getTokenLogin();
+    try {
+        return await api(token)
+            .get(`/v${versao}`)
+            .then((respose) => {
+                if (respose.data !== null) {
+                    return respose.data;
+                }
+            }).catch((error) => {
+                console.log(`Erro na requisição da API andpoint rootEntryPoint!`);
+                if (error.response) {
+                    return {
+                        codigo: error.response.status,
+                        erro: error.response.data.error,
+                        mensagem: error.response.data.error_description,
+                    }
+                } else {
+                    throw error;
+                }
+            });
+    } catch (error) {
+        console.log(`Erro no método rootEntryPoint do arquivo UsuarioService -> ${new Date()} -> erro: ${error}`);
+    }
+}
+
 export const salvarTokenLogin = async (access_token, expires_in) => {
     try {
         await AsyncStorage.setItem('@access_token', access_token);
@@ -51,31 +104,5 @@ export const salvarTokenLogin = async (access_token, expires_in) => {
         await AsyncStorage.setItem('@data', JSON.stringify(new Date()));
     } catch (error) {
         console.log(`Erro no método salvarTokenLogin do arquivo UsuarioService -> ${new Date()} -> erro: ${error}`);
-    }
-}
-
-export const rootEntryPoint = async () => {
-    const token = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3Vhcmlvc19pZCI6MSwidXNlcl9uYW1lIjoicGF1bGlzdGVuc2V0ZWNub2xvZ2lhQGdtYWlsLmNvbSIsInNjb3BlIjpbIlJFQUQiLCJXUklURSJdLCJleHAiOjE2MzgxNzE5ODcsImF1dGhvcml0aWVzIjpbIkVESVRBUl9DTElFTlRFUyIsIkNPTlNVTFRBUl9VU1VBUklPU19HUlVQT1NfUEVSTUlTU09FUyIsIkVESVRBUl9DSURBREVTIiwiRURJVEFSX0ZPUk1BU19QQUdBTUVOVE8iLCJHRVJFTkNJQVJfUEVESURPUyIsIkVESVRBUl9FTVBSRVNBUyIsIkVESVRBUl9VU1VBUklPU19HUlVQT1NfUEVSTUlTU09FUyIsIkdFUkVOQ0lBUl9IT1NUUyIsIkNPTlNVTFRBUl9QRURJRE9TIiwiRURJVEFSX0VTVEFET1MiXSwianRpIjoiZTE5NWFlN2EtMDgxYy00NjI1LWE3OWMtNjE0YTJhNmYxMTM4Iiwibm9tZV9jb21wbGV0byI6IkFkbWluaXN0cmFkb3IiLCJjbGllbnRfaWQiOiJteXN0b3JlLWFwcCJ9.NrhK_sqryguTwhl-pYKWzQ-iKDqO6zhbk7q27d1IHdJRK2dv99Jg2QT6ekSYVtbo_jtfLyuWHuJV7Iv96YAJ5uGO76ovuY7LTlMH_Rt2iIE761pBc8aepOzSQN7nkLHJyAWeOcAJAfhC8o5A33fBlaC5XWMRwOYbbO50Ymxk6F60URN8PYWXT6102f5pbU1w_ndkMD5nnlhQsxp3H2n_7d-IytphO7cp8h82pMSFza17_pp4iK5rlNNPyVBoXr5x3a7CQUW5A79P3mYBKw90YiFH6DWbiWhRN9T9opPREyPXwmocni-IwZhemXc_BsheSH0qJQ3G3epbFlx_rDjN4A`;
-    try {
-        return await api(token)
-        .get('/v1')
-        .then((respose) => {
-            if (respose.data !== null) {
-                return respose.data;
-            }
-        }).catch((error) => {
-            console.log(`Erro na requisição da API andpoint rootEntryPoint!`);
-            if (error.response) {
-                return {
-                    codigo: error.response.status,
-                    erro: error.response.data.error,
-                    mensagem: error.response.data.error_description,
-                }
-            } else {
-                throw error;
-            }
-        });
-    } catch (error) {
-        console.log(`Erro no método rootEntryPoint do arquivo UsuarioService -> ${new Date()} -> erro: ${error}`);
     }
 }
