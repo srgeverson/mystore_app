b2ce1e2a4aee565d5bd0e265658388c9d5512338
˜GeversonSouza <geversonjosedesouza@gmail.com>
2021-12-02 23:01:13 -0300

    Clean code

--- src/core/routes/DrawerNavigator.js
+++ src/core/routes/DrawerNavigator.js
@@ -34,6 +34,15 @@ const DrawerNavigator = () => {
         }
     }
 
+    const getIcones = (icon, size, focused) => {
+        try {
+            return <Icon name={icon ? icon : 'exclamation-triangle'} size={size ? size : 20} color={focused ? '#007bff' : '#6c757d'} />
+        } catch (error) {
+            console.log(`Erro no método getIcones do arquivo DrawerNavigator -> ${new Date()} -> erro: ${error}`);
+            return <Icon name={icon ? icon : 'exclamation-triangle'} size={size ? size : 20} color={'#dc3545'} />
+        }
+    }
+
     useEffect(() => {
         getEndpoints();
     }, []);
@@ -48,15 +57,16 @@ const DrawerNavigator = () => {
             drawerStatusBarAnimation: 'fade'
         }} drawerContent={props => <DrawerCustom {...props} />}>
             <Drawer.Screen name="BemVindo" component={BemVindoStackNavigator} options={{
-                headerShown: true, title: 'MyStore', drawerIcon: (focused) => <Icon name='home' size={20} color={focused ? '#007bff' : '#6c757d'} />,
+                headerShown: true, title: 'MyStore', drawerIcon: (focused) => getIcones('home', null, focused),
+                headerTitleAlign: 'center',
             }} />
-            {menusDisponiveis && <Drawer.Screen name="ListarCidades" component={ListarCidadesStackNavigator} options={{ headerShown: true, title: 'Lista de Cidades', drawerIcon: (focused) => <Icon name='building' size={20} color={focused ? '#007bff' : '#6c757d'} />, }} />}
+            {menusDisponiveis && <Drawer.Screen name="ListarCidades" component={ListarCidadesStackNavigator} options={{ headerShown: true, title: 'Lista de Cidades', drawerIcon: (focused) => getIcones('building', null, focused), }} />}
             {/* {menusDisponiveis && <Drawer.Screen name="ListarEmpresas" component={ListarEmpresasStackNavigator} options={{ headerShown: true, title: 'Lista de Empresas',drawerIcon: (focused)=><Icon name='home' size={20} color={focused? '#007bff' : '#6c757d'}/>  , }} />} */}
-            {menusDisponiveis && <Drawer.Screen name="ListarEstados" component={ListarEstadosStackNavigator} options={{ headerShown: true, title: 'Lista de Estados', drawerIcon: (focused) => <Icon name='globe' size={20} color={focused ? '#007bff' : '#6c757d'} />, }} />}
+            {menusDisponiveis && <Drawer.Screen name="ListarEstados" component={ListarEstadosStackNavigator} options={{ headerShown: true, title: 'Lista de Estados', drawerIcon: (focused) => getIcones('globe', null, focused), }} />}
             {/* {menusDisponiveis && <Drawer.Screen name="ListarFormasPagamento" component={ListarFormasPagamentoStackNavigator} options={{ headerShown: true, title: 'Lista de Formas de Pagamento',drawerIcon: (focused)=><Icon name='home' size={20} color={focused? '#007bff' : '#6c757d'}/>  , }} />} */}
             {/* {menusDisponiveis && <Drawer.Screen name="ListarGrupos" component={ListarGruposStackNavigator} options={{ headerShown: true, title: 'Lista de Grupos',drawerIcon: (focused)=><Icon name='sign-out' size={20} color={focused? '#007bff' : '#6c757d'}/>  , }} />} */}
-            {menusDisponiveis && <Drawer.Screen name="ListarPermissoes" component={ListarPermissoesStackNavigator} options={{ headerShown: true, title: 'Lista de Permissões', drawerIcon: (focused) => <Icon name='unlock-alt' size={20} color={focused ? '#007bff' : '#6c757d'} />, }} />}
-            {menusDisponiveis && <Drawer.Screen name="ListarUsuarios" component={ListarUsuariosStackNavigator} options={{ headerShown: true, title: 'Lista de Usuarios', drawerIcon: (focused) => <Icon name='users' size={20} color={focused ? '#007bff' : '#6c757d'} />, }} />}
+            {menusDisponiveis && <Drawer.Screen name="ListarPermissoes" component={ListarPermissoesStackNavigator} options={{ headerShown: true, title: 'Lista de Permissões', drawerIcon: (focused) => getIcones('unlock-alt', null, focused), }} />}
+            {menusDisponiveis && <Drawer.Screen name="ListarUsuarios" component={ListarUsuariosStackNavigator} options={{ headerShown: true, title: 'Lista de Usuarios', drawerIcon: (focused) => getIcones('users', null, focused), }} />}
         </Drawer.Navigator>
     );
 };
