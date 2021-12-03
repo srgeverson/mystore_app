df6c8cfffed9f131dcb99252d1415c6856f9835f
˜GeversonSouza <geversonjosedesouza@gmail.com>
2021-12-03 00:25:38 -0300

    Personalizado a barra de tarefas

--- src/core/routes/DrawerNavigator.js
+++ src/core/routes/DrawerNavigator.js
@@ -5,6 +5,9 @@ import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerI
 import { BemVindoStackNavigator, ListarCidadesStackNavigator, ListarEstadosStackNavigator, ListarPermissoesStackNavigator, ListarUsuariosStackNavigator } from './StackNavigator';
 import { AuthorityContext } from '../contexts';
 import { rootEntryPoint } from '../../services/UsuarioService';
+import { Button } from 'react-native-elements';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 
 const Drawer = createDrawerNavigator();
 
@@ -20,15 +23,19 @@ const DrawerCustom = (props) => {
     );
 }
 
-const DrawerNavigator = () => {
+const DrawerNavigator = (props) => {
 
     const [menusDisponiveis, setMenusDisponiveis] = useState(null);
+
+    const [offline, setOffLine] = useState(true);
 
     const getEndpoints = async () => {
         try {
             const entryPoint = await rootEntryPoint();
-            if (entryPoint._links)
+            if (entryPoint._links){
+                setOffLine(false);
                 setMenusDisponiveis(entryPoint._links);
+            }
         } catch (error) {
             console.log(`Erro no método getEndpoints do arquivo DrawerNavigator -> ${new Date()} -> erro: ${error}`);
         }
@@ -40,6 +47,43 @@ const DrawerNavigator = () => {
         } catch (error) {
             console.log(`Erro no método getIcones do arquivo DrawerNavigator -> ${new Date()} -> erro: ${error}`);
             return <Icon name={icon ? icon : 'exclamation-triangle'} size={size ? size : 20} color={'#dc3545'} />
+        }
+    }
+    const getHeaderLeft = () => {
+        try {
+            return (<Button
+                icon={
+                        <Icon
+                            onPress={() => alert('ops')}
+                            name="bars"
+                            size={20}
+                            color='#FFF' />
+                }
+            />);
+        } catch (error) {
+            console.log(`Erro no método getHeaderLeft do arquivo DrawerNavigator -> ${new Date()} -> erro: ${error}`);
+        }
+    }
+    const getHeaderRight = () => {
+        try {
+            return (<Button
+                icon={
+                    offline ?
+                        <Icon
+                            onPress={() => Alert.alert('Dispositivo off-line', 'Dados serão sincronizados com o servidor quando voltar a internet!')}
+                            name="warning"
+                            size={20}
+                            color='#FFFF00' />
+                        :
+                        <Icon
+                            onPress={() => Alert.alert('Dispositivo on-line', 'Dados estão sincronizados com o servidor!')}
+                            name="refresh"
+                            size={20}
+                            color='#90EE90' />
+                }
+            />);
+        } catch (error) {
+            console.log(`Erro no método getHeaderRight do arquivo DrawerNavigator -> ${new Date()} -> erro: ${error}`);
         }
     }
 
@@ -59,6 +103,11 @@ const DrawerNavigator = () => {
             <Drawer.Screen name="BemVindo" component={BemVindoStackNavigator} options={{
                 headerShown: true, title: 'MyStore', drawerIcon: (focused) => getIcones('home', null, focused),
                 headerTitleAlign: 'center',
+                headerTitleStyle: {color: '#FFF'},
+                //headerLeft: () => getHeaderLeft(),
+                headerLeft: () => <Button title='teste' onPress={()=>navigation.openDrawer()}/>,
+                headerRight: () => getHeaderRight(),
+                headerStyle:{backgroundColor: '#1B8BD1',}
             }} />
             {menusDisponiveis && <Drawer.Screen name="ListarCidades" component={ListarCidadesStackNavigator} options={{ headerShown: true, title: 'Lista de Cidades', drawerIcon: (focused) => getIcones('building', null, focused), }} />}
             {/* {menusDisponiveis && <Drawer.Screen name="ListarEmpresas" component={ListarEmpresasStackNavigator} options={{ headerShown: true, title: 'Lista de Empresas',drawerIcon: (focused)=><Icon name='home' size={20} color={focused? '#007bff' : '#6c757d'}/>  , }} />} */}
