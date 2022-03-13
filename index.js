4f00da53368dc14e3891e3a2a37758393b4808ee
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-03-12 21:50:39 -0300

    alterando nome do método.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './src/core/app';
-import {buscaTodosDados} from './src/core/synchronize';
+import {atualizandoDadosLocais} from './src/core/synchronize';
 import Database from './src/core/database';
 
 AppRegistry.registerComponent(appName, () => {
     Database.initDB();
-    buscaTodosDados();
+    atualizandoDadosLocais();
     return App
 });
