c3f2bfdf68fce9cd48afbd39908358b120a8f7a7
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-03-12 21:49:17 -0300

    Removendo lógica de teste

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './src/core/app';
-import {buscaTodosDados, atualizandoToken} from './src/core/synchronize';
+import {buscaTodosDados} from './src/core/synchronize';
 import Database from './src/core/database';
 
 AppRegistry.registerComponent(appName, () => {
-    //Database.dropDatabase();
     Database.initDB();
     buscaTodosDados();
-    // atualizandoToken(9);
     return App
 });
