1699b4f863e48be7eb38415bc78db38f7fac4181
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-04-24 14:32:40 -0300

    Identando código.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import App from './src/core/app';
-import {atualizandoDadosLocais,atualizandoToken} from './src/core/synchronize';
+import { atualizandoDadosLocais, atualizandoToken } from './src/core/synchronize';
 import Database from './src/core/database';
 
 AppRegistry.registerComponent(appName, () => {
