55d65d817eb42739aa20d657bef1750c8b450cfa
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-05-16 02:14:17 -0300

    Validação de dados qual vazio

--- src/views/components/Utils/index.js
+++ src/views/components/Utils/index.js
@@ -1,18 +1,21 @@
 const createGuid = () => {
-    function S4() {
+    s4 = () => {
         return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     }
-    return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+    return (s4() + s4() + "-" + s4() + "-4" + s4().substr(0, 3) + "-" + s4() + "-" + s4() + s4() + s4()).toLowerCase();
 }
 
 const keyExtractor = (item, index) => index.toString();
 
 const extractorFirstLeterNames = (value) => {
-    let leter = value.split(' ');
-    if (leter.length > 1)
-        return leter[0].substr(0, 1) + leter[1].substr(0, 1);
-    else
-        return value.substr(0, 1)
+    if (value) {
+        let leter = value.split(' ');
+        if (leter.length > 1)
+            return leter[0].substr(0, 1).toUpperCase() + leter[1].substr(0, 1).toUpperCase();
+        else
+            return value.substr(0, 1).toUpperCase();
+    } else
+        return '):';//Campo indefinido/nulo
 }
 
 export { createGuid, extractorFirstLeterNames, keyExtractor }
