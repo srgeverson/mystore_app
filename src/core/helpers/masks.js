94882591510d6b9dfb70968c40fbbf98592a580b
Geverson <geversonjosedesouza@gmail.com>
2022-05-31 23:01:09 -0300

    Helpers de formatação e validação de campos

---
+++ src/core/helpers/masks.js
@@ -0,0 +1,43 @@
+
+export const celularMask = (value) => {
+    return value
+        .replace(/\D+/g, '')
+        .replace(/^(\d{2})(\d)/g, "($1) $2")
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{4})\d+?$/, '$1');
+}
+
+export const cepMask = (value) => {
+    return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1');
+}
+
+export const cnpjMask = (value) => {
+    return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{2})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1/$2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1');
+}
+
+export const cpfMask = (value) => {
+    return value
+        .replace(/\D+/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1');
+}
+
+export const telefoneMask = (value) => {
+    return value
+        .replace(/\D+/g, '')
+        .replace(/^(\d{2})(\d)/g, "($1) $2")
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(-\d{4})\d+?$/, '$1');
+}
