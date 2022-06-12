11373193e7f5c030a3fc639b89da20780353e440
Geverson <geversonjosedesouza@gmail.com>
2022-06-12 14:39:48 -0300

    Método para upload dos dados locais

--- src/core/synchronize/index.js
+++ src/core/synchronize/index.js
@@ -9,10 +9,10 @@ export const atualizandoDadosLocais = async () => {
     setInterval(async () => {
         console.log(`Iniciando a sincronização dos dados -> ${new Date()}...`);
         await sincronizarCidade(token);
-        await sincronizarCliente(token, empresa),
-            await sincronizarEstado(token)
+        await sincronizarCliente(token, empresa);
+        await sincronizarEstado(token);
         console.log(`Finalizando a sincronização dos dados -> ${new Date()}...`);
-    }, 60000);
+    }, 120000);
 }
 
 export const atualizandoToken = async () => {
@@ -22,24 +22,6 @@ export const atualizandoToken = async () => {
         setTimeout(() => {
             buscarEAtualizarRefreshToken();
             setInterval(() => {
-                buscarEAtualizarRefreshToken();
-            }, dadosTempoDeAtualizacaoToken.tempoDeSincronizacaoDoToken);
-        }, expira);
-    }
-}
-
-export const atualizandoTokenOLD = async () => {
-    var dadosTempoDeAtualizacaoToken = await calculaTempoDeAtualizacaoToken();
-    //console.log(`Dados de atualizacao do token = ${JSON.stringify(dadosTempoDeAtualizacaoToken)}`);
-    let expira = dadosTempoDeAtualizacaoToken.dataRestanteMilisegundos;
-    if (expira > 0) {
-        //console.log(`Token será atualizado em ${expira} milisegundos ${new Date()}`);
-        setTimeout(() => {
-            //console.log(`Atualizando token primeira vez ${new Date()}`);
-            buscarEAtualizarRefreshToken();
-            setInterval(() => {
-                //console.log(`Atualizando token ${new Date()}`);
-                //console.log(`Tempo token local ${expira}`);
                 buscarEAtualizarRefreshToken();
             }, dadosTempoDeAtualizacaoToken.tempoDeSincronizacaoDoToken);
         }, expira);
@@ -66,5 +48,16 @@ export const atualizandoDadosLocaisHeaderRight = async (atualizandoDados) => {
         await sincronizarEstado(token);
         console.log(`Finalizando a sincronização dos dados -> ${new Date()}...`);
         atualizandoDados(false);
+    }, 60000);
+}
+
+export const enviandoDadosLocais = async () => {
+    const { empresa, token } = await getLoginSalvo();
+    setInterval(async () => {
+        console.log(`Iniciando o envio dos dados locais -> ${new Date()}...`);
+        // await sincronizarCidade(token);
+        // await sincronizarCliente(token, empresa);
+        // await sincronizarEstado(token);
+        console.log(`Finalizando o envio dos dados locais -> ${new Date()}...`);
     }, 60000);
 }
