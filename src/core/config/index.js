c7f1f23d56823c30ed919477524eb6044ad31259
Geverson <geversonjosedesouza@gmail.com>
2022-06-12 14:39:12 -0300

    alteração no IP local

--- src/core/config/index.js
+++ src/core/config/index.js
@@ -1,4 +1,4 @@
-export const url = (process.env.PUBLIC_URL) ? "http://mystore-app.ddns.net:8282" : "http://192.168.0.102:8080";
+export const url = (process.env.PUBLIC_URL) ? "http://mystore-app.ddns.net:8282" : "http://192.168.0.103:8080";
 export const user = (process.env.PUBLIC_URL) ? "mystore-app" : "mystore-app";
 export const pass = (process.env.PUBLIC_URL) ? "123321" : "123321";
 export const user_manager = (process.env.PUBLIC_URL) ? "mystore-manager" : "mystore-manager";
